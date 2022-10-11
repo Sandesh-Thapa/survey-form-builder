50ee51fc3a9d48a5e6ccc172f31584aebfd58644
Sandesh Thapa <sandesh@nerdplatoon.com>
2022-10-11 13:33:39 +0545

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
-// import CreateForm from './pages/CreateForm';
+import CreateForm from './pages/CreateForm';
 import Homepage from './pages/Homepage';
-// import SurveyForm from './components/SurveyForm'
 import './App.css'
-import FormBuilder from './components/FormBuilder';
 
 function App() {
   return (
-    // <SurveyForm />
     <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container">
-              <Link className="navbar-brand" to="#">Form Builder</Link>
+              <Link className="navbar-brand" to="/">Form Builder</Link>
           </div>
       </nav>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        {/* <Route path="/create-form" element={<CreateForm />} /> */}
-        <Route path="/create-form" element={<FormBuilder />} />
+        <Route path="/create-form" element={<CreateForm />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/CreateForm.js
+++ src/pages/CreateForm.js
@@ -1,28 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import SurveyForm from '../components/SurveyForm'
-import DisplayTest from '../components/DisplayTest'
+import FormBuilder from '../components/FormBuilder'
 
 const CreateForm = () => {
-  return (
-    <div className='bg-light min-h-100v'>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarColor01">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <Link to="/" className="nav-link" >Back</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <DisplayTest />
-    </div>
-  )
+  return <FormBuilder />
 }
 
 export default CreateForm
