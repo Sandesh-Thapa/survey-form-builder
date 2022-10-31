eb991e675df06adc66bf79a29bf65c3460179cd9
Sandesh Thapa <sandesh@nerdplatoon.com>
2022-10-31 15:56:36 +0545

    Code optimized

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,23 +4,8 @@ export const generateFormType = (type) => {
     const defaultAttr = {isRequired: false}
     switch(type) {
         case "text":
-            return {
-                id: uuidv4(),
-                type,
-                ...defaultAttr
-            }
         case "dropdown":
-            return {
-                id: uuidv4(),
-                type,
-                ...defaultAttr
-            }
         case "rating":
-            return {
-                id: uuidv4(),
-                type,
-                ...defaultAttr
-            }
         case "boolean":
             return {
                 id: uuidv4(),
